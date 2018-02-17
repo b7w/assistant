089da1d60c292a32acb671b60bd7df265361f360
b7w@isudo.ru <b7w@isudo.ru>
2018-02-17 13:41:08 +0300

    Modify CI

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,6 +14,7 @@ def checkoutStage() {
 def testStage() {
     docker.image('python:3.6-slim').inside {
         stage('Test') {
+            sh('pip install3 -r requirements.txt')
             withEnv(['ETH_WALLETS=addr1,addr2']) {
                 sh('cd src && python -m unittest')
             }
