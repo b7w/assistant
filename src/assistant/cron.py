8f7ec6afc6dc9073cab309d56b75af0643aae6df
b7w@isudo.ru <b7w@isudo.ru>
2018-03-27 23:26:16 +0300

    Remove morning notifications

--- src/assistant/cron.py
+++ src/assistant/cron.py
@@ -11,21 +11,6 @@ from assistant.telegram import bot
 logger = logging.getLogger(__name__)
 
 
-@aiocron.crontab('30 9 * * 1-5', start=False)
-async def notify_morning_weather():
-    logger.info('Notify morning weather')
-    for user_id in core.NOTIFICATION_CONSUMERS:
-        try:
-            private = bot.private(user_id)
-            message = await core.weather()
-            await private.send_text('Доброе утро!')
-            await private.send_text(message)
-            message = await core.wallets(core.ETH_WALLETS)
-            await private.send_text(message)
-        except Exception as e:
-            logger.exception(e)
-
-
 @aiocron.crontab('00 21 * * 1-5', start=False)
 async def notify_money_rates():
     logger.info('Notify money rate')
@@ -42,7 +27,6 @@ async def notify_money_rates():
 
 async def main():
     try:
-        notify_morning_weather.start()
         notify_money_rates.start()
     except Exception as e:
         logger.exception(e)
